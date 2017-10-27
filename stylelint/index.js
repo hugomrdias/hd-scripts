ec5106302e6474884f86a5c441a33e8acb131b90
Hugo Dias <hugomrdias@gmail.com>
2017-10-27 15:39:10 +0100

    feat: add lint and format

---
+++ stylelint/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = {
+    extends: [
+        'stylelint-config-idiomatic-order',
+        'prettier-stylelint/config.js'
+    ],
+    rules: {
+        'indentation': 4,
+        'string-quotes': 'single'
+    }
+};
