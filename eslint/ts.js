7bcf8ad70bb6499dd8e30645698d00a6f261b18a
Hugo Dias <hugomrdias@gmail.com>
2023-09-13 18:18:54 +0100

    fix: find the nearest tsconfig.json

--- eslint/ts.js
+++ eslint/ts.js
@@ -4,8 +4,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     sourceType: 'module',
-    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/parser/README.md#parseroptionsproject
-    project: './tsconfig.json',
+    project: true,
   },
   extends: [
     './js',
